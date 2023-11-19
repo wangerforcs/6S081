c7f92283c88cdf6ac629ee5b1dedd0733328d01b
wangerforcs <3541174576@qq.com>
2023-11-19 16:53:48 +0800

    syscall

--- user/trace.c
+++ user/trace.c
@@ -1,16 +1,27 @@
+#include "kernel/param.h"
 #include "kernel/types.h"
 #include "kernel/stat.h"
-#include "kernel/fcntl.h"
 #include "user/user.h"
 
-int main(int argc, char*argv[])
+int
+main(int argc, char *argv[])
 {
-    if(argc < 3){
-        fprintf(2, "usage: trace mask command\n");
-        exit(1);
-    }
-    int mask = atoi(argv[1]);
-    trace(mask);
-    exec(argv[2], argv+2);
-    exit(0);
+  int i;
+  char *nargv[MAXARG];
+
+  if(argc < 3 || (argv[1][0] < '0' || argv[1][0] > '9')){
+    fprintf(2, "Usage: %s mask command\n", argv[0]);
+    exit(1);
+  }
+
+  if (trace(atoi(argv[1])) < 0) {
+    fprintf(2, "%s: trace failed\n", argv[0]);
+    exit(1);
+  }
+  
+  for(i = 2; i < argc && i < MAXARG; i++){
+    nargv[i-2] = argv[i];
+  }
+  exec(nargv[0], nargv);
+  exit(0);
 }
