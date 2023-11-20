b676fb2a0e40b54b8372216b49e4f92680c14ff2
wangerforcs <3541174576@qq.com>
2023-11-21 00:06:50 +0800

    traps

--- kernel/proc.c
+++ kernel/proc.c
@@ -132,13 +132,6 @@ found:
     return 0;
   }
 
-  if((p->usys = (struct usyscall*)kalloc()) ==0 ){
-    freeproc(p);
-    release(&p->lock);
-    return 0;
-  }
-  p->usys->pid = p->pid;
-
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -165,9 +158,6 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
-  if(p->usys)
-    kfree((void*)p->usys);
-  p->usys = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -212,14 +202,6 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
-  if(mappages(pagetable, USYSCALL, PGSIZE,
-              (uint64)(p->usys), PTE_R | PTE_U) < 0){
-    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
-    uvmunmap(pagetable, TRAPFRAME, 1, 0);
-    uvmfree(pagetable, 0);
-    return 0;
-  }
-
   return pagetable;
 }
 
@@ -230,7 +212,6 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
-  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -1,7 +1,7 @@
 #include "types.h"
 #include "riscv.h"
-#include "param.h"
 #include "defs.h"
+#include "param.h"
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
@@ -54,8 +54,9 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-
   argint(0, &n);
+  if(n < 0)
+    n = 0;
   acquire(&tickslock);
   ticks0 = ticks;
   while(ticks - ticks0 < n){
@@ -68,29 +69,6 @@ sys_sleep(void)
   release(&tickslock);
   return 0;
 }
-
-
-#ifdef LAB_PGTBL
-uint64 sys_pgaccess(void){
-  uint64 addr,abits;
-  int nums;
-  argaddr(0,&addr);
-  argint(1,&nums);
-  argaddr(2,&abits);
-  unsigned int mask = 0;
-  for(int i=0;i<nums;i++){
-    pte_t* pte = walk(myproc()->pagetable,addr + i * PGSIZE,0);
-    if(pte == 0)
-      return -1;
-    mask |= (( (*pte & PTE_A) ? 1 : 0) << i);  
-    // why?
-    *pte &= ~PTE_A;
-  }
-  if(copyout(myproc()->pagetable, abits, (char*)&mask, sizeof(mask)) < 0)
-    return -1;
-  return 0; 
-}
-#endif
 
 uint64
 sys_kill(void)
